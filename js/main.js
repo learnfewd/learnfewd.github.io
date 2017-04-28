b27da73d821c0f713eefca41b8a5da5366a39de7
learnfewd <kareem.o.taylor@gmail.com>
2017-04-27 21:24:55 -0700

    More updates

--- js/main.js
+++ js/main.js
@@ -11,21 +11,21 @@ $('.NewReview').hide();
 })*/
 
 $('.Review').on('click', function(){
-    $('form').toggle(3000);
-	$('.NewReview').toggle(3000);
+    $('.contact').toggle(300);
+	$('.NewReview').toggle(300);
 });
 
 $('.NewReview').on('click', function(){
-    $('aside').toggle(3000);
+    $('aside').toggle(300);
 });
 
 
 $('.Chew1').on('click', function(){
-    $('.Menu').toggle(3000);
+    $('li').toggle(300);
 });
 
 $('.Chew2').on('click', function(){
-    $('.Time').toggle(3000);
+    $('.Time').toggle(300);
 });
 
 /*$('.NewReview').on('click', function(){
